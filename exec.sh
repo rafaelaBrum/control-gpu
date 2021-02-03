8b5276f5dad99d77162d800d9df6da3b1badf0ed
rafaelaBrum <rafinhacbrum@gmail.com>
2021-02-03 12:44:31 -0300

    Adding the without failure to automatic tests

--- exec.sh
+++ exec.sh
@@ -12,6 +12,18 @@ export NOTIFY_PWD='R1357908642@'
 export POSTGRES_USER=postgres
 export POSTGRES_PASS=rafaela123
 echo " ----------------------------
+ Without Failure
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+        echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        python3 client.py control
+        sleep 5m
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
   Failure rate of 1/(2 hours)
  ---------------------------- "
 COUNTER=1
@@ -19,8 +31,8 @@ while [  $COUNTER -lt 4 ]; do
         echo " ----------------------------
   Running test - Counter=$COUNTER
  ----------------------------"
-#        python3 client.py --revocation_rate 0.0001388888888888889 control
-#        sleep 5m
+        python3 client.py --revocation_rate 0.0001388888888888889 control
+        sleep 5m
         COUNTER=$((COUNTER+1))
 done
 echo " ----------------------------
@@ -31,8 +43,8 @@ while [  $COUNTER -lt 4 ]; do
 	echo " ----------------------------
   Running test - Counter=$COUNTER
  ----------------------------"
-#	python3 client.py control
-#	sleep 5m
+	python3 client.py control 6.944444444444444e-05
+	sleep 5m
 	COUNTER=$((COUNTER+1))
 done
 echo " ----------------------------
