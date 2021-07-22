972508b11118b015c12e863903f2dadd402c6439
Rafaela Brum <rafinhacbrum@gmail.com>
2021-07-22 20:51:26 -0300

    Creating JSON for all COVID comparisons

---
+++ exec_covid.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo " ----------------------------
+  Starting docker
+ ----------------------------"
+docker start pg-gpu-docker
+echo " ----------------------------
+  Exporting variables
+ ----------------------------"
+export SETUP_FILE=setup.cfg
+export SETUP_PATH=$HOME/control-gpu/
+export NOTIFY_PWD='R1357908642@'
+export POSTGRES_USER=postgres
+export POSTGRES_PASS=rafaela123
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+        echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        python3 client.py control
+        sleep 5m
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+  Stopping docker
+ ----------------------------"
+docker stop pg-gpu-docker
+echo " ----------------------------
+  Finished
+ ----------------------------"
+
