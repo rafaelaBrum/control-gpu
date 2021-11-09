e8151d41b5a2e8bcc4ce7add7efdc8df3c66338b
Rafaela Brum <rafinhacbrum@gmail.com>
2021-11-09 12:51:09 +0100

    Fixing VM setup script for centralized approach

--- setup/vms_setup/segframe.sh
+++ setup/vms_setup/segframe.sh
@@ -18,7 +18,7 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/
 sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get update
-sudo apt-get install cuda-10-0
+sudo apt-get install cuda-10-0 -y
 echo "CUDA 10.0 installed"
 
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb
@@ -38,8 +38,18 @@ echo "cuDNN 7 installed"
 sudo apt install git -y
 git clone https://www.github.com/rafaelaBrum/Segframe.git
 cd Segframe
-pip3 -r requirements.txt
+pip3 install -U pip
+pip3 install -r requirements.txt
 echo "Segframe cloned and requirements installed"
+
+# Installing fuse for GCP
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gcsfuse -y
+echo "GCSFuse installed"
+
 
 echo "Reboot machine to test the nvidia-smi command and then creates a image of it"
 
