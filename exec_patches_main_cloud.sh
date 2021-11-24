9a71efe29079de3e704f9f4c8b0acc08899526fc
Rafaela Brum <rafinhacbrum@gmail.com>
2021-11-24 12:50:20 -0300

    Tests with breast cancer patches

--- exec_patches_main_cloud.sh
+++ exec_patches_main_cloud.sh
@@ -11,7 +11,7 @@ export SETUP_PATH=$HOME/control-gpu/
 export NOTIFY_PWD='R1357908642@'
 export POSTGRES_USER=postgres
 export POSTGRES_PASS=rafaela123
-epochs=20
+epochs=25
 echo " ----------------------------
  ${epochs} epochs
  ---------------------------- "
@@ -25,7 +25,7 @@ do
   COUNTER=1
   neural_network="Inception"
   dataset_folder='brca'
-  while [  $COUNTER -lt 2 ]; do
+  while [  $COUNTER -lt 4 ]; do
     echo " ----------------------------
     Running test - Counter=$COUNTER
    ----------------------------"
@@ -35,7 +35,8 @@ do
     #sleep 30m
     COUNTER=$((COUNTER+1))
   done
-donesplit="0.96 0.02 0.02"
+done
+split="0.96 0.02 0.02"
 underscore_split=$(echo $split | sed 's/ /_/g')
 echo " ----------------------------
  Train_Valid_Test: ${underscore_split}
@@ -45,13 +46,13 @@ do
   COUNTER=1
   neural_network="Inception"
   dataset_folder='brca'
-  while [  $COUNTER -lt 2 ]; do
+  while [  $COUNTER -lt 4 ]; do
     echo " ----------------------------
     Running test - Counter=$COUNTER
    ----------------------------"
    FOLDER="patches_${dataset_folder}_${epochs}_epochs_${neural_network}_split_${underscore_split}_$((COUNTER))_folder_${instance_type}"
     echo python3 test_patches_CNN_TIL_without_fl_cloud.py --folder $FOLDER --instance_type $instance_type --dataset_folder $dataset_folder --neural_network $neural_network --split $split --epochs $epochs
-    python3 test_patches_CNN_TIL_without_fl_cloud.py --folder $FOLDER --instance_type $instance_type --dataset_folder $dataset_folder --neural_network $neural_network --split $split --epochs $epochs
+#    python3 test_patches_CNN_TIL_without_fl_cloud.py --folder $FOLDER --instance_type $instance_type --dataset_folder $dataset_folder --neural_network $neural_network --split $split --epochs $epochs
     #sleep 30m
     COUNTER=$((COUNTER+1))
   done
@@ -66,13 +67,13 @@ do
   COUNTER=1
   neural_network="Inception"
   dataset_folder='brca'
-  while [  $COUNTER -lt 2 ]; do
+  while [  $COUNTER -lt 4 ]; do
     echo " ----------------------------
     Running test - Counter=$COUNTER
    ----------------------------"
    FOLDER="patches_${dataset_folder}_${epochs}_epochs_${neural_network}_split_${underscore_split}_$((COUNTER))_folder_${instance_type}"
     echo python3 test_patches_CNN_TIL_without_fl_cloud.py --folder $FOLDER --instance_type $instance_type --dataset_folder $dataset_folder --neural_network $neural_network --split $split --epochs $epochs
-    python3 test_patches_CNN_TIL_without_fl_cloud.py --folder $FOLDER --instance_type $instance_type --dataset_folder $dataset_folder --neural_network $neural_network --split $split --epochs $epochs
+#    python3 test_patches_CNN_TIL_without_fl_cloud.py --folder $FOLDER --instance_type $instance_type --dataset_folder $dataset_folder --neural_network $neural_network --split $split --epochs $epochs
     #sleep 30m
     COUNTER=$((COUNTER+1))
   done
