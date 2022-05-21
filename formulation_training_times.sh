b28aa283fdd3f53d6e9d86f512c26aa80825fc5d
Rafaela Brum <rafinhacbrum@gmail.com>
2022-05-21 20:37:53 +0200

    Testing more with dataset in AWS

--- formulation_training_times.sh
+++ formulation_training_times.sh
@@ -12,9 +12,9 @@ export NOTIFY_PWD='R1357908642@'
 export POSTGRES_USER=postgres
 export POSTGRES_PASS=rafaela123
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/bustling-icon-331608-97742b8ca898.json
-FOLDER_TESTS=input/FederatedLearning/mathematical_formulation/VGG_DS_GCP
-COUNTER=2
-while [  $COUNTER -lt 4 ]; do
+FOLDER_TESTS=input/FederatedLearning/mathematical_formulation/VGG_DS_AWS
+COUNTER=16
+while [  $COUNTER -lt 19 ]; do
   echo " ----------------------------
    Running test - Counter=$COUNTER
   ----------------------------"
