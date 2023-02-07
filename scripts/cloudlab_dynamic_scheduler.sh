c666bce3e9cdc83d9227fad90807b6560e9528bd
Rafaela Brum <rafinhacbrum@gmail.com>
2023-02-07 11:41:14 -0300

    Creating tests for faulty server, faulty clients, and all faulty

--- scripts/cloudlab_dynamic_scheduler.sh
+++ scripts/cloudlab_dynamic_scheduler.sh
@@ -11,7 +11,111 @@ export SETUP_PATH=$HOME/control-gpu/
 export NOTIFY_PWD='R1357908642@'
 export POSTGRES_USER=postgres
 export POSTGRES_PASS=rafaela123
-cp setup_cloudlab/4_clients_50_rounds_dynamic_scheduler.cfg setup.cfg
+echo " ----------------------------
+4 clients running 50 rounds
+---------------------------- "
+echo " ----------------------------
+Test Case 1 (server and clients are faulty)
+---------------------------- "
+cp setup_cloudlab/4_clients_50_rounds_dyn_sched_all.cfg setup.cfg
+echo " ----------------------------
+  Failure rate of 1/(2 hours)
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+        echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        echo python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 0.0001388888888888889
+        python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 0.0001388888888888889
+        if [ $COUNTER -ne 3 ]; then
+          sleep 5m
+        fi
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+  Failure rate of 1/(4 hours)
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+	echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        echo python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
+        python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
+        if [ $COUNTER -ne 3 ]; then
+          sleep 5m
+        fi
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+  Failure rate of 1/(6 hours)
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+	echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        echo python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 4.6296296296296294e-05
+        python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 4.6296296296296294e-05
+        if [ $COUNTER -ne 3 ]; then
+          sleep 5m
+        fi
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+Test Case 2 (only server is faulty)
+---------------------------- "
+cp setup_cloudlab/4_clients_50_rounds_dyn_sched_server.cfg setup.cfg
+echo " ----------------------------
+  Failure rate of 1/(2 hours)
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+        echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        echo python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 0.0001388888888888889
+        python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 0.0001388888888888889
+        if [ $COUNTER -ne 3 ]; then
+          sleep 5m
+        fi
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+  Failure rate of 1/(4 hours)
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+	echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        echo python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
+        python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
+        if [ $COUNTER -ne 3 ]; then
+          sleep 5m
+        fi
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+  Failure rate of 1/(6 hours)
+ ---------------------------- "
+COUNTER=1
+while [  $COUNTER -lt 4 ]; do
+	echo " ----------------------------
+  Running test - Counter=$COUNTER
+ ----------------------------"
+        echo python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 4.6296296296296294e-05
+        python3 client.py control --emulated --strategy FedAvgSave --revocation_rate 4.6296296296296294e-05
+        if [ $COUNTER -ne 3 ]; then
+          sleep 5m
+        fi
+        COUNTER=$((COUNTER+1))
+done
+echo " ----------------------------
+Test Case 3 (only clients are faulty)
+---------------------------- "
+cp setup_cloudlab/4_clients_50_rounds_dyn_sched_cli.cfg setup.cfg
 echo " ----------------------------
   Failure rate of 1/(2 hours)
  ---------------------------- "
