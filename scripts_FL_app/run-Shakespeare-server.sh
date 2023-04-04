2d368d793da7647d8aa988b1c727303daebb7d5f
Rafaela Brum <rafinhacbrum@gmail.com>
2023-04-04 15:25:52 -0300

    Testing FedAvgSave in new application

--- scripts_FL_app/run-Shakespeare-server.sh
+++ scripts_FL_app/run-Shakespeare-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SERVER_FILE=$HOME/control-gpu/control/daemon/TF_shakespeare_server.py
 N_CLIENTS=1
-STRATEGY=FedAvgSave
+STRATEGY='FedAvgSave --frequency_ckpt 1'
 
 python3 $SERVER_FILE --rounds 20 --sample_fraction 1 --min_sample_size $N_CLIENTS \
  --min_num_clients $N_CLIENTS --strategy $STRATEGY
