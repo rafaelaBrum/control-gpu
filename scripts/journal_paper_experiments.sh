e7eab9c043604bf206e69bbca08f5a674feabebc
Rafaela Brum <rafinhacbrum@gmail.com>
2024-05-13 18:47:36 -0300

    Adding revocation rate of 1 hour

--- scripts/journal_paper_experiments.sh
+++ scripts/journal_paper_experiments.sh
@@ -38,7 +38,7 @@ cp setup_journal_paper/test_case_setup_simulation_checkpoint_all_aws.cfg setup.c
 echo " ----------------------------
   Failure rate of 1/(2 hours)
  ---------------------------- "
-COUNTER=1
+COUNTER=4
 while [  $COUNTER -lt 4 ]; do
         echo " ----------------------------
   Running test - Counter=$COUNTER
@@ -51,15 +51,15 @@ while [  $COUNTER -lt 4 ]; do
         COUNTER=$((COUNTER+1))
 done
 echo " ----------------------------
-  Failure rate of 1/(4 hours)
+  Failure rate of 1/(1 hour)
  ---------------------------- "
-COUNTER=1
+COUNTER=3
 while [  $COUNTER -lt 4 ]; do
 	echo " ----------------------------
   Running test - Counter=$COUNTER
  ----------------------------"
-        echo python3 client.py control --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
-        python3 client.py control --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
+        echo python3 client.py control --strategy FedAvgSave --revocation_rate 0.0002777777777777778
+        python3 client.py control --strategy FedAvgSave --revocation_rate 0.0002777777777777778
         if [ $COUNTER -ne 3 ]; then
           sleep 30m
         fi
@@ -85,15 +85,15 @@ while [  $COUNTER -lt 4 ]; do
         COUNTER=$((COUNTER+1))
 done
 echo " ----------------------------
-  Failure rate of 1/(4 hours)
+  Failure rate of 1/(1 hour)
  ---------------------------- "
 COUNTER=1
 while [  $COUNTER -lt 4 ]; do
 	echo " ----------------------------
   Running test - Counter=$COUNTER
  ----------------------------"
-        echo python3 client.py control --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
-        python3 client.py control --strategy FedAvgSave --revocation_rate 6.944444444444444e-05
+        echo python3 client.py control --strategy FedAvgSave --revocation_rate 0.0002777777777777778
+        python3 client.py control --strategy FedAvgSave --revocation_rate 0.0002777777777777778
         if [ $COUNTER -ne 3 ]; then
           sleep 30m
         fi
