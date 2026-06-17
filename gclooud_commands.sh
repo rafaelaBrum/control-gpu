eca0e32b8295a5b6e54f67d2223253eff210bbec
Rafaela Brum <rafaelabrum@eng.uerj.br>
2026-06-17 15:45:05 -0300

    Flower empty app executing in GCP without Framework

--- gclooud_commands.sh
+++
@@ -1,6 +0,0 @@
-#enviar algo para a VM:
-gcloud compute scp PATH_TO_FILE  --zone INSTANCE_ZONE INSTANCE_NAME:~
-#acessar a VM:
-gcloud compute ssh --zone INSTANCE_ZONE INSTANCE_NAME
-
-#nome usuário: sa_109649273287045369425 (o número é o client_id da conta de servico)
---
+++ gclooud_flower_commands.sh
@@ -0,0 +1,15 @@
+#enviar algo para a VM:
+gcloud compute scp PATH_TO_FILE  --zone INSTANCE_ZONE INSTANCE_NAME:~
+#acessar a VM:
+gcloud compute ssh --zone INSTANCE_ZONE INSTANCE_NAME
+
+#nome usuário: sa_109649273287045369425 (o número é o client_id da conta de servico)
+
+#SuperLink command (one in the server VM)
+flower-superlink --control-api-address 0.0.0.0:9093 --serverappio-api-address 0.0.0.0:9091 --fleet-api-address 0.0.0.0:9092 --insecure
+
+#SuperNode command (one per client)
+flower-supernode --insecure --superlink IP_SERVER:9092 --clientappio-api-address 0.0.0.0:9094
+
+#Execution command - in the server VM
+flwr run . local-deployment --stream
